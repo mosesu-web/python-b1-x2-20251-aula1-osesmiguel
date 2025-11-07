c5e318a3457610d29334ec1a68fe514a258bcf70
mosesu-web <mosesu@uoc.edu>
2025-11-07 20:16:33 +0100

    Completar las clases Person, Buyer y Seller con los atributos pertinentes
    Buyer y Seller usan la clase abstracta Person como interfaz.
    Falta por incluir doctring

--- bills/entity.py
+++ bills/entity.py
@@ -1,15 +1,17 @@
 from abc import ABC, abstractmethod
 
 class Person:
-    def __init__(self, dni: str, email: str, mobile: str):
+    def __init__(self, dni: str, email: str, mobile: str) -> None:
         # Write here your code
-        pass
+        self.dni: str = dni
+        self.email: str = email
+        self.mobile: str = mobile
 
     @abstractmethod
     def print(self):
         pass
 
-    def __eq__(self, another):
+    def __eq__(self, another: object) -> bool:
        # Do not change this method
        return hasattr(another, 'dni') and self.dni == another.dni
     
@@ -20,7 +22,10 @@ class Person:
 class Buyer(Person):
     def __init__(self, dni: str, email: str, mobile: str, full_name: str, age: int, address: str):
         # Write here your code
-        pass
+        super().__init__(dni, email, mobile)
+        self.full_name: str = full_name
+        self.age: int = age
+        self.address: str = address
 
     def print(self):
         # Do not change this method
@@ -28,9 +33,11 @@ class Buyer(Person):
 
 class Seller(Person):
     # Write the parameters in the next line
-    def __init__():
+    def __init__(self, dni: str, email: str, mobile: str, business_name: str, business_address: str):
         # Write here your code        
-        pass
+        super().__init__(dni, email, mobile)
+        self.business_name: str = business_name
+        self.business_address: str = business_address
         
     def print(self):
         # Do not change this method
